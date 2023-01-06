cd7a4ecbd934d2c449015de5837e4eee1ea6b5c5
Ranjana Chaudhary <ranjana20@navgurukul.org>
2023-01-07 03:41:40 +0530

    changes

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -32,10 +32,10 @@ export default function Home() {
                 <img src={logoFigma} className="figmalogo" />
                 <div className='border-2 h-25  nav backdrop-blur-lg'>
                     <div className='flex font-lg text-white'>
-                        <span className='p-3'><u className='p-5'>Home</u></span>
-                        <span className='p-3'>Our Industrius</span>
+                        <span className='lg:p-3'><u className='lg:p-5'>Home</u></span>
+                        <span className='lg:p-3'>Our Industrius</span>
                         <span className='mt-4'><ExpandMoreIcon style={{ fontSize: '2rem' }} /></span>
-                        <span className='p-3'>Our Company</span>
+                        <span className='lg:p-3'>Our Company</span>
                         <span className='mt-4'><ExpandMoreIcon style={{ fontSize: '2rem' }} /></span>
                     </div>
                     <div className='right-nav text-white'>
@@ -50,7 +50,7 @@ export default function Home() {
             </div>
             <div className='home'>
                 {/* <h5>Home</h5> */}
-                <div className="relative top-56" >
+                <div className="relative lg:top-56 xs:top-72" >
                     <Carousel responsive={responsive} interval={7000} dynamicHeight={true} showDots={true} className='mt-2' swipeable={true} infiniteLoop={false} autoPlay={false}>
                         <div className='item  ' >
                             <h1 className='text-gradient-to-r from-yello-400'>RAMAERA </h1>
@@ -63,12 +63,14 @@ export default function Home() {
                             <h1 className='text-gradient-to-r from-yello-400'>RAMAERA </h1>
                             <h1 className='text-gradient-to-r from-yello-400'>INDUSTRIES</h1>
                             <h4>An Incredible multi-industrial approach orianted<br />towords financial independant,custmor focus and<br /> serving the best quality to the people</h4>
+                            <button className='more-btn'>Explore More</button>
                             {/* <img className='h-25'  src="https://researchleap.com/wp-content/uploads/2021/09/Usine-du-futur_0-scaled.jpg" /> */}
                         </div>
                        <div className='item  ' >
                             <h1 className='text-gradient-to-r from-yello-400'>RAMAERA </h1>
                             <h1 className='text-gradient-to-r from-yello-400'>INDUSTRIES</h1>
                             <h4>An Incredible multi-industrial approach orianted<br />towords financial independant,custmor focus and<br /> serving the best quality to the people</h4>
+                            <button className='more-btn'>Explore More</button>
                             {/* <img className='h-25'  src="https://researchleap.com/wp-content/uploads/2021/09/Usine-du-futur_0-scaled.jpg" /> */}
                         </div>
                     </Carousel>
